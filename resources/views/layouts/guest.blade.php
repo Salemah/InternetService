d6af65b3c6c63e0dfd59d1bf07147b688d66c281
Salemah <kadertanvir1@gmail.com>
2023-05-04 21:00:48 +0600

    Full Project

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,29 @@
+<?php
+    $generalSettings = App\Models\Admin\Settings\GeneralSetting::first();
+?>
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('auth_title')</title>
+
+    <link href="{{ asset('backend/auth/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/auth/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+<div class="container">
+    <div class="card card-container">
+        <img id="profile-img" class="profile-img-card" src="{{ asset('img/profile.png') }}" />
+
+        {{ $slot }}
+    </div><!-- /card-container -->
+</div><!-- /container -->
+
+<script src="{{ asset('backend/auth/js/bootstrap.min.js') }}"></script>
+</body>
+</html>
+
